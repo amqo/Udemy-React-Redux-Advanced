90cb3a7d601eff3c6a5e444905d9790083bf8372
Alberto <alberto.commit@github.com>
2016-04-19 12:19:42 +0200

    Finish testing for comment box component. Make it a form

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 
 export default class CommentBox extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment : ''
+    };
+  }
+
   render() {
     return (
-      <div className="comment-box">
-        <textarea />
-        <button>Submit comment</button>
-      </div>
+      <form onSubmit={ this.onHandleSubmit.bind(this) } className="comment-box">
+        <textarea
+          value={ this.state.comment }
+          onChange={ this.onHandleChange.bind(this) } />
+        <button action="submit">Submit comment</button>
+      </form>
     );
+  }
+
+  onHandleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      comment: ''
+    });
+  }
+
+  onHandleChange(event) {
+    this.setState({
+      comment: event.target.value
+    });
   }
 }
