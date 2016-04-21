90945e9f9090915c7c3304875e9eaad6a165caeb
Alberto <alberto.commit@github.com>
2016-04-21 12:35:29 +0200

    Added header and resources components, and wire them with Router

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+    return (
+      <div>
+        Super Special Recipe
+        <ul>
+          <li>Secret element 1</li>
+          <li>Secret element 2</li>
+          <li>Secret element 3</li>
+          <li>Secret element 4</li>
+        </ul>
+      </div>
+    );
+};
