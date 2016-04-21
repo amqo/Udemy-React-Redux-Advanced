0d8af2061877fa5fad7ed949d54a50c003024231
Alberto <alberto.commit@github.com>
2016-04-21 16:19:02 +0200

    Navigate to home page when trying to render Resources through HOC, when user is not authenticated

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    checkIfAuthenticated(props) {
+      if (!props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillMount() {
+      this.checkIfAuthenticated(this.props);
+    }
+
+    componentWillUpdate(nextProps) {
+      this.checkIfAuthenticated(nextProps);
+    }
+
     render() {
       return <ComposedComponent { ...this.props }/>
     }
   }
 
-  return Authentication;
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
