e0a734dde6b653fd94dcbabf284d8df1e8ab28a1
Alberto Muñoz <alberto.mqo@gmail.com>
2016-05-12 16:43:31 +0200

    Added HOC to check auth before showing Feature component.

---
+++ client/src/components/auth/require_auth.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    checkIfAuthenticated(props) {
+      console.log('User is authenticated?', props.authenticated);
+      if (!props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+    componentWillMount() {
+      this.checkIfAuthenticated(this.props);
+    }
+    componentWillUpdate(nextProps) {
+      this.checkIfAuthenticated(nextProps);
+    }
+
+    render() {
+      return <ComposedComponent { ...this.props }/>
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.auth.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
+}
